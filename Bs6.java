fce18b0c5286c2700ef1f8fd9fb17603dd5b869e
purush2300 <reddypurushothm@gmail.com>
2026-03-25 21:22:42 +0530

    Initial commit

---
+++ Bs6.java
@@ -0,0 +1,47 @@
+public class Bs6 {
+static int  lowbound(int[]a,int target){
+        int i=0;
+        int ans=a.length;
+        int j=a.length-1;
+        while(i<=j){
+            int mid=(i+j)/2;
+            if(a[mid]>=target){
+                ans=mid;
+                j=mid-1;
+            }
+            else{
+                i=mid+1;
+            }
+        }
+        return ans;
+    }
+
+
+static int upperbound(int[]a,int target){
+    int i=0;
+    int ans2=a.length;
+    int j=a.length-1;
+    while(i<=j){
+        int mid=(i+j)/2;
+        if(a[mid]>target){
+            ans2=mid;
+            j=mid-1;
+        }
+        else{
+            i=mid+1;
+        }
+    }
+    return ans2;
+}
+
+
+    public static void main(String[] args) {
+        int []a={5,5,5,5,5,5,5};
+        int target=5;
+        int count=0;
+  int lb=lowbound(a, target);
+  int up=upperbound(a, target);
+  System.out.println(up-lb);
+
+    }
+}
